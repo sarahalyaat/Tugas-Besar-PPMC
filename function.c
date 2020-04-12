1d508d818d8366c4f25106b3468daa878cdd2b69
sarah.tsab <alya.sarah.rara@gmail.com>
2020-04-12 17:21:05 +0700

    Update searchVal.c -> function.c

---
+++ function.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct node{
+	char word[15];
+	struct node *next;
+} node;
+
+typedef struct queue{
+	node *front, *rear;
+} queue;
+
+//Test
+node* randomHead(node **text,int n){
+	int r = (rand() % n) + 1;
+	int i = 0;
+	node *head = *text;
+	printf("%d\n",r);
+
+	for(i = 0; i < r; i++){
+		head = head->next;
+	}
+	return (head);
+}
+
+//Fungsi mencari value dari key pada text
+void searchValue(node **text, queue **key, int n){
+	node *currText = randomHead(text,n);
+	node *testText;
+	node *currKey;
+	int found = 0;
+	while (found == 0){
+		printf("%s\n",currText->word);
+		currKey = (*key)->front;
+		testText = currText;
+		while(currKey!=NULL){
+			if (strcmp(testText->word,currKey->word)!=0)
+				break;
+			currKey = currKey->next;
+			testText = testText->next;
+		}
+		if (currKey==NULL){
+			found = 1;
+		}
+		else {
+			currText = currText->next;
+		}
+	}
+	printf("val: %s ",testText->word);
+	// dequeue(key);
+	// enqueue(key, testText->word);
+}
+
+
+
